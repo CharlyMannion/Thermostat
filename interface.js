7aa1371a2a34d5a3e8907f2487a0fa3a37e643ac
Charly Mannion <charlottemannion100@gmail.com>
2018-05-17 15:52:05 +0100

    adding listeners to the interface

--- interface.js
+++ interface.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+
   $('#temperature').text(thermostat.temperature);
+
+  $('#energy-usage').text(thermostat.energyUsage());
+
+  $('#temperature-up').on('click', function(){
+    thermostat.increaseTemperature();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('#temperature-down').on('click', function(){
+    thermostat.decreaseTemperature();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('#temperature-reset').on('click', function(){
+    thermostat.resetTemperature();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('#power-saving').on('click', function(){
+    thermostat.togglePowerSaving();
+    $('#power-saving').text(thermostat.isPowerSaving);
+  });
+
 });
