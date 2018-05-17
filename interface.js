b451559ae12f3ae472af78197390c30ceeb59144
aseelm <aseelmustafa@outlook.com>
2018-05-17 16:45:21 +0100

    adds weather to interface

--- interface.js
+++ interface.js
@@ -7,15 +7,18 @@ $(document).ready(function() {
 
   $('#temperature-up').on('click', function(){
     thermostat.increaseTemperature();
+    $('#energy-usage').text(thermostat.energyUsage());
     $('#temperature').text(thermostat.temperature);
   });
 
   $('#temperature-down').on('click', function(){
     thermostat.decreaseTemperature();
+    $('#energy-usage').text(thermostat.energyUsage());
     $('#temperature').text(thermostat.temperature);
   });
 
   $('#temperature-reset').on('click', function(){
+    $('#energy-usage').text(thermostat.energyUsage());
     thermostat.resetTemperature();
     $('#temperature').text(thermostat.temperature);
   });
@@ -23,6 +26,10 @@ $(document).ready(function() {
   $('#power-saving').on('click', function(){
     thermostat.togglePowerSaving();
     $('#power-saving').text(thermostat.isPowerSaving);
+  });
+
+  $.get('http:/api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6ad17e7426ddf72d0a8756bcec7b1981', function(data) {
+    $('.weather').text(data["weather"][0]["description"]);
   });
 
 });
