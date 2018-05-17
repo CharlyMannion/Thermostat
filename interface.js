d75193bb55cf7efffbd4628ea71807d551d253ed
Charly Mannion <charlottemannion100@gmail.com>
2018-05-17 19:57:37 +0100

    select city fuction implemented on thermostat

--- interface.js
+++ interface.js
@@ -28,8 +28,18 @@ $(document).ready(function() {
     $('#power-saving').text(thermostat.isPowerSaving);
   });
 
-  $.get('http:/api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6ad17e7426ddf72d0a8756bcec7b1981', function(data) {
-    $('.weather').text(data["weather"][0]["description"]);
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=6ad17e7426ddf72d0a8756bcec7b1981&units=metric', function(data) {
+      $('.weather').text(data["weather"][0]["description"]);
+    });
+  });
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=6ad17e7426ddf72d0a8756bcec7b1981&units=metric', function(data) {
+      $('.temperature').text(data.main.temp);
+    });
   });
 
 });
